3906b060fad371baf8bc5fdea43c6bab4887a578
yiting2004 <125672506+yiting2004@users.noreply.github.com>
2023-04-30 16:26:24 +0800

    Add files via upload

---
+++ A1113307_0421_1.java
@@ -0,0 +1,6 @@
+public class A1113307_0421_1 {
+    public static void main(String[] args){
+        String email="a1113307@mail.nuk.edu.tw";
+        System.out.println(email.matches("[a,m]{1}[0-9]{7}[@]{1}[a-z]{4}[.]{1}[a-z]{3}[.]{1}[a-z]{3}[.]{1}[a-z]{2}"));
+    }
+}
