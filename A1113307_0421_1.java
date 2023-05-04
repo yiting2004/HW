aad9abd4ea8c3d80b2f2d7ddc3f1b1a9be5d4f17
yiting2004 <125672506+yiting2004@users.noreply.github.com>
2023-05-05 00:56:27 +0800

    Update A1113307_0421_1.java

--- A1113307_0421_1.java
+++ A1113307_0421_1.java
@@ -1,6 +1,14 @@
+import java.util.*;
 public class A1113307_0421_1 {
     public static void main(String[] args){
-        String email="a1113307@mail.nuk.edu.tw";
-        System.out.println(email.matches("[a,m]{1}[0-9]{7}[@]{1}[a-z]{4}[.]{1}[a-z]{3}[.]{1}[a-z]{3}[.]{1}[a-z]{2}"));
+        String email;
+        System.out.println("請輸入你的學校email:");
+        Scanner num=new Scanner(System.in);
+        email=num.next();
+        if(email.matches("[a,m]{1}[0-9]{7}[@]{1}[a-z]{4}[.]{1}[a-z]{3}[.]{1}[a-z]{3}[.]{1}[a-z]{2}")){
+            System.out.println("Your email:"+email);
+        }else{
+            System.out.println("格式錯誤");
+        }
     }
 }
